defd069cb67edd53a0d7798af951b93e55423313
tunnckoCore <mameto_100@mail.bg>
2017-02-11 23:18:49 +0200

    fix(rollup): fix bundles

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 const zopfli = require('rollup-plugin-zopfli')
 const uglify = require('rollup-plugin-uglify')
+const resolve = require('rollup-plugin-node-resolve')
+const commonjs = require('rollup-plugin-commonjs')
 
 module.exports = {
   entry: 'index.js',
@@ -7,6 +9,8 @@ module.exports = {
   sourceMap: true,
   useStrict: false,
   plugins: [
+    commonjs(),
+    resolve(),
     uglify({ compress: { warnings: false } }),
     zopfli({ options: { numiterations: 1000 } })
   ],
